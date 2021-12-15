8ee56236bdfe9c821c04a4c601490a0d588dc3e0
jurandarjanovic <95487867+jurandarjanovic@users.noreply.github.com>
2021-12-15 21:28:15 +0100

    Update Python_prvi_zadatak.py

--- Python_prvi_zadatak.py
+++ Python_prvi_zadatak.py
@@ -1,24 +1,9 @@
-"""" Zadatak 1
+
 
 a=input("Upisi prvi broj: ")
 b=input("Upisi drugi broj: ")
 print("Umnožak dva upisana broja je: ")
 print(int(a)*int(b))
 
-""" 
-""" Zadatak 2
-a=input("Upisi jedan broj: ")
-if int(a)%2==0:
-  print("Broj je paran")
-  print("-----")
-  if int(a)<0:
-    print("Broj je negativan")
-    print("-----")
-  else:
-    print("Broj je pozitivan")
-    print("-----")
-else:
-  print("Broj je neparan")
-  print("-----")
 
-"""
+
