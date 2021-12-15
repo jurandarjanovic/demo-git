bf82a77f8284bbb25aef9111414fe71b20d13eba
jurandarjanovic <darjan.juranovic@gmail.com>
2021-12-15 22:17:04 +0100

    Create Python_drugi_zadatak.py

---
+++ Python_drugi_zadatak.py
@@ -0,0 +1,22 @@
+a=input("Upiši jedan broj: ")
+if int(a)==0:
+  print("Upisani broj je nula")
+  print("----------")
+
+if int(a)%2==0:
+  print("Broj je paran")
+  print("----------")
+else:
+  print("Broj nije paran")
+  print("----------")
+ 
+if int(a)<0 and int(a) !=0:
+  print("Broj je negativan")
+  print("----------")
+
+if int(a)>0 and int(a) !=0:
+  print("Broj je pozitivan")
+  print("----------")
+from time import *
+
+sleep(3)
