6dea72d9cdd817dcb1a7b7e4493c4d29253859b0
jurandarjanovic <darjan.juranovic@gmail.com>
2021-12-15 21:41:38 +0100

    Update Python_prvi_zadatak.py

--- Python_prvi_zadatak.py
+++ Python_prvi_zadatak.py
@@ -2,6 +2,7 @@ a=input("Upisi prvi broj: ")
 b=input("Upisi drugi broj: ")
 print("Umnožak dva upisana broja je: ")
 print(int(a)*int(b))
+print("**********************************")
 input("Pritisni bilo koju tipku za izlaz")
 
 
